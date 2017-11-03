e02ff8cc7620e764bfeef79884695a8365820e4d
ndricca <andrea.ricca@hotmail.it>
2017-11-03 18:48:13 +0100

    daily adjustments

--- analisi_rese.R
+++ analisi_rese.R
@@ -1,6 +1,5 @@
 library(reshape2)
 library(ggplot2)
-<<<<<<< HEAD
 library(dplyr)
 library(tidyr)
 library(plotly)
@@ -21,20 +20,3 @@ plt <- ggplot(df,aes(Data_frangitura,Resa_perc,color = Comune_oliveto)) +
   facet_wrap(~Provincia_oliveto)
 
 ggplotly(plt)
-=======
-
-df <- read.csv2("rese_frantoi.csv")
-df <- df[,c("Provincia_oliveto","Resa_perc","Data_frangitura")]
-df$Data_frangitura <- as.Date(df$Data_frangitura)
-
-df$Comune_oliveto <- tolower(df$Comune_oliveto)
-
-ifelse(df$Comune_oliveto=="",df$Provincia_oliveto,df$Comune_oliveto)
-
-
-ggplot(df,aes(Data_frangitura,Resa_perc,color = Comune_oliveto)) + 
-  geom_line() +
-  facet_wrap(~Provincia_oliveto)
-
-
->>>>>>> 448da257c551156780813b99027c850d14066cda
